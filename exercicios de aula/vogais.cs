c197654342b5ac738b44dce1db6f85f84976b89a
Eric Verglio <ericvergilio@gmail.com>
2017-05-03 16:05:07 -0300

    vogais

---
+++ exercicios de aula/vogais.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nv;
+            double km, cv;
+            string n, cla, sla;;
+            Console.WriteLine("qual a quantidade de veículos do galpão?");
+            nv = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < nv; i++)
+            {
+                Console.WriteLine("qual o nome do carro?");
+                n = Console.ReadLine();
+
+                Console.WriteLine("qual é a potência do veiculo?");
+                cv = Convert.ToDouble(Console.ReadLine());
+
+
+                Console.WriteLine("quantos quilometros rodados?");
+                km = Convert.ToDouble(Console.ReadLine());
+
+            }
+        }
+        public static string classificar (string n, double km, double cv, string cla, string sla)
+        {
+            
+
+            if (km < 5000)
+                cla = "novo";   
+
+            else if (km < 30000)
+                cla = "semi-novo";
+
+            else
+                cla = "velho";
+
+            if (cv > 200)
+                sla = "potente";
+
+            else if (cv < 200)
+                sla = "forte";
+
+            else
+
+                sla = "popular";
+
+
+            return string.Format("{0} - {1} {2}", n, cla, sla);
+
+        }
+
+
+
+           
+        
+    }
+}
